4a7aa3c663291fd2f0d5410848c0c083ef530181
Devin Fischer <devinfischer32@gmail.com>
2021-08-31 14:35:21 -0600

    added access token

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: 'dd5e2edd25464362993ae2531bdcde42',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const app = express()
 
