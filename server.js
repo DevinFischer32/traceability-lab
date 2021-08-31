79a83f2c526d8b61781dd0e61de22ecbca5e588c
Devin Fischer <devinfischer32@gmail.com>
2021-08-31 15:22:12 -0600

    Added backend

--- server.js
+++ server.js
@@ -9,11 +9,32 @@ let rollbar = new Rollbar({
 })
 
 const app = express()
-
+const usernames = []
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('Html loaded')
 })
+
+app.post('/api/username', (req, res)=>{
+    let {username} = req.body
+    username = username.trim()
+
+    const index = usernames.findIndex(userName=> usersName === username)
+
+    if(index === -1 && username !== ''){
+        usernames.push(username)
+        rollbar.log('username added successfully', {author: 'Devin', type: 'manual entry'})
+        res.status(200).send(usernames)
+    } else if (username === ''){
+        rollbar.error('No username given')
+        res.status(400).send('must provide a username.')
+    } else {
+        rollbar.error('username already exists')
+        res.status(400).send('that username already exists')
+    }
+
+})
+
 
 const port = process.env.PORT || 4545
 
